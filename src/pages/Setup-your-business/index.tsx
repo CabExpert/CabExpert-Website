d923f83b388d8a3536ede7b7bdd7245b678fbd60
abhishekda2 <abhishekda22s@gmail.com>
2024-04-11 19:24:56 +0530

    signup

---
+++ src/pages/Setup-your-business/index.tsx
@@ -0,0 +1,129 @@
+import Navbar from "@/component/navbar";
+import styles from "@/styles/setup-your-business.module.scss";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Setup() {
+  const [typeToggle, setTypeToggle] = useState(false);
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <h2>Almost done! Setup your business</h2>
+        <form>
+          <div className={styles.logoUpload}>
+            <label htmlFor="logo">
+              <img src="/company_logo_placeholder.png" alt="Company Logo" />
+            </label>
+            <input type="file" id="logo" />
+          </div>
+          <div className={styles.setupform}>
+            <div>
+              <input
+                type="text"
+                id="companyName"
+                className={styles.inputField}
+                placeholder="Company Name"
+              />
+            </div>
+            <div>
+              <input
+                type="tel"
+                id="phoneNumber"
+                className={styles.inputField}
+                placeholder="Phone Number"
+              />
+            </div>
+            <div>
+              <div className={styles.box}>
+                <p tabIndex={0}>Verification</p>
+                <p tabIndex={1}>Enter your OTP code number:</p>
+                <div className={styles.inputContainer}>
+                  <input
+                    type="text"
+                    className={styles.input}
+                    style={{ width: "300px" }}
+                  />
+                  <button className={styles.verifyButton}>Verify</button>
+                </div>
+                <p>Verification code is sent to this number:</p>
+              </div>
+            </div>
+            <div className={styles.type_parent}>
+              <div
+                className={styles.business_type}
+                onClick={() => setTypeToggle(!typeToggle)}
+              >
+                <h3>Business Type</h3>
+                <Image
+                  src="/arrow-down.png"
+                  alt="arrow-down"
+                  width={16}
+                  height={16}
+                />
+              </div>
+              {typeToggle && (
+                <div>
+                  <div className={styles.toggle_parent}>
+                    <label htmlFor="regi">Registered</label>
+                    <input type="radio" name="business" id="regi" />
+                  </div>
+                  <div className={styles.toggle_parent}>
+                    <label htmlFor="unreg">Unregistered</label>{" "}
+                    <input type="radio" name="business" id="unreg" />
+                  </div>
+                </div>
+              )}
+            </div>
+            <div>
+              <input
+                type="GST number"
+                id="GST number"
+                className={styles.inputField}
+                placeholder="GST number"
+              />
+            </div>
+            <div>
+              <input
+                type="Address"
+                id="Address"
+                className={styles.inputField}
+                placeholder="Address"
+              />
+            </div>
+            <div>
+              <input
+                type="Pin"
+                id="Pin"
+                className={styles.inputField}
+                placeholder="Pin"
+              />
+            </div>
+            <div>
+              <input
+                type="City"
+                id="City"
+                className={styles.inputField}
+                placeholder="City"
+              />
+            </div>
+            <div className={styles.country}>
+              <input
+                type="Country"
+                id="Country"
+                className={styles.inputField}
+                placeholder="Country"
+              />
+              <Image
+                src="/arrow-down.png"
+                alt="dropdown"
+                width={16}
+                height={16}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,71 @@
+import Navbar from "@/component/navbar";
+import styles from "@/styles/Signup.module.scss";
+
+export default function Signup() {
+  return (
+    <>
+      <Navbar />
+
+      <div className={`${styles.register} container `}>
+        <h2>REGISTRATION</h2>
+        <form className={styles.form}>
+          <div className={styles.name}>
+            <input
+              type="text"
+              id="FirstName"
+              name="FirstName"
+              placeholder="First Name"
+              required
+            />
+
+            <input
+              type="text"
+              id="LastName"
+              name="LastName"
+              placeholder="Last Name"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Re-enter Password"
+              required
+            />
+            <div className={styles.check}>
+              <input type="checkbox" style={{ width: 12, height: 12,}} />
+              
+              <p>
+                By signing up, I agree with the <span>Terms of Use</span> &{" "}
+                <span>Privacy Policy</span>
+              </p>
+            </div>
+          </div>
+          <button type="submit" className={styles.continuebutton}>
+            Continue
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
